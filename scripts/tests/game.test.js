f6ade59046a36cde527ce6dc00b9abf538e4f7bf
Odden69 <helena.joh2@gmail.com>
2022-02-05 08:52:23 +0000

    initial commit

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+beforeAll(() => {
+     let fs = require('fs');
+     let fileContents = fs.readFileSync('index.html', 'utf-8');
+     document.open();
+     document.write(fileContents);
+     document.close();
+ })
+
+ describe('game object contains correct keys', () => {
+     test('score key exist', () => {
+         expect('score' in game).toBe(true);
+     })
+ })
